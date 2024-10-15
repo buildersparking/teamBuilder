c560608bafd5c18ff65cde7175282669bda32c53
buildersparking <builder.sparkingzero@gmail.com>
2024-10-15 13:46:43 +0200

    5

--- src/App.js
+++ src/App.js
@@ -5,8 +5,14 @@ import './App.css';
 
 function App() {
   useEffect(() => {
+    console.log("Début de l'initialisation de Google Analytics");
     ReactGA.initialize('9829337711');
+    console.log("Google Analytics initialisé avec l'ID : 9829337711");
+    
     ReactGA.send({ hitType: "pageview", page: window.location.pathname });
+    console.log("Pageview envoyé pour : " + window.location.pathname);
+    
+    console.log("Fin de l'initialisation de Google Analytics");
   }, []);
 
   return (
